f71308002b3d247a5a112ecc470cd088dfcfa50d
liyi <liyi235235>
2019-11-20 13:35:46 +0800

    add

--- first-demo/go.mod
+++ first-demo/go.mod
@@ -5,5 +5,7 @@ go 1.13
 require (
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
-	golang.org/x/crypto v0.0.0-20191029031824-8986dd9e96cf // indirect
+	github.com/urfave/cli v1.22.1
+	github.com/urfave/cli/v2 v2.0.0-alpha.2
+	golang.org/x/crypto v0.0.0-20191117063200-497ca9f6d64f // indirect
 )
--- first-demo/main.go
+++ first-demo/main.go
@@ -2,22 +2,114 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"regexp"
+	"time"
 
 	"github.com/labstack/echo"
+	cli "github.com/urfave/cli/v2"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", handlerindex)
-	fmt.Println("starting echo")
-	err := e.Start(":8080")
-	if err != nil {
-		fmt.Println("echo", err)
-	}
+	cliCmd(os.Args)
 }
 
 func handlerindex(c echo.Context) error {
 	fmt.Println("Hello world handlerindex")
 	return c.JSON(http.StatusOK, `{"hello":"world"}`)
+}
+
+func cliCmd(args []string) {
+	app := &cli.App{
+		Name:     "Sub_Cmd",
+		Version:  "v1.0.0",
+		Compiled: time.Now(),
+		Authors: []*cli.Author{
+			&cli.Author{
+				Name:  "Toney",
+				Email: "lyxmq@126.com",
+			},
+		},
+		Copyright: "(c) 2019 Toney",
+		Usage:     "Cli sub commands test",
+		UsageText: "sub_cmd [global options] command [command options] [arguments...]",
+		ArgsUsage: "[args and such]",
+
+		Commands: []*cli.Command{
+			{
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "add a task to the list",
+				Action: func(c *cli.Context) error {
+					fmt.Println("added task: ", c.Args().First())
+					return nil
+				},
+			},
+			{
+				Name:    "port",
+				Aliases: []string{"p"},
+				Usage:   "http listen port",
+				Action: func(c *cli.Context) error {
+					// fmt.Println("added task: ", c.Args().First())
+					port := c.Args().First()
+					pattern := `^(\d+)$`
+					result, _ := regexp.MatchString(pattern, port)
+					if !result {
+						fmt.Printf("port must is number %s", port)
+					} else {
+						httpBoostrap(port)
+					}
+					return nil
+				},
+			},
+			{
+				Name:    "complete",
+				Aliases: []string{"c"},
+				Usage:   "complete a task on the list",
+				Action: func(c *cli.Context) error {
+					fmt.Println("completed task: ", c.Args().First())
+					return nil
+				},
+			},
+			{
+				Name:    "template",
+				Aliases: []string{"t"},
+				Usage:   "options for task templates",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "add",
+						Usage: "add a new template",
+						Action: func(c *cli.Context) error {
+							fmt.Println("new task template: ", c.Args().First())
+							return nil
+						},
+					},
+					{
+						Name:  "remove",
+						Usage: "remove an existing template",
+						Action: func(c *cli.Context) error {
+							fmt.Println("removed task template: ", c.Args().First())
+							return nil
+						},
+					},
+				},
+			},
+		},
+	}
+	err := app.Run(args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func httpBoostrap(port string) {
+	e := echo.New()
+	e.GET("/", handlerindex)
+	fmt.Println("starting echo")
+	err := e.Start(":" + port)
+	if err != nil {
+		fmt.Println("echo", err)
+	}
 }
